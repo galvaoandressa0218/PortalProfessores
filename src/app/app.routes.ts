92c15a08391636eca7ce8c14894597b1b6e45afb
galvaoandressa0218 <galvaoandressa0218@gmail.com>
2025-09-26 18:07:31 -0300

    atualização

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+import { PageLoginComponent } from './features/page-login/page-login.component';
+import { ForgotPasswordComponent } from './features/forgot-password/forgot-password.component';
+import { CourseListComponent } from './features/courses/course-list/course-list.component';
+import { SubjectListComponent } from './features/subjects/subject-list/subject-list.component';
+import { TeacherListComponent } from './features/teachers/teacher-list/teacher-list.component'; 
+
+export const routes: Routes = [
+    { path: 'login', component: PageLoginComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path:'forgot-password', component: ForgotPasswordComponent },
+    { path: 'courses', component: CourseListComponent },
+    { path: 'subjects', component: SubjectListComponent },
+    { path: 'teachers', component: TeacherListComponent }
+];
+
+export const AppRoutes = RouterModule.forRoot(routes);
+
+NgModule ({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+
---
+++ src/app/features/page-login/page-login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-page-login',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './page-login.component.html',
+  styleUrl: './page-login.component.css'
+})
+export class PageLoginComponent {
+
+}
